88444a7a89652e3d24c0efe3d054a2529be54cc8
Samuel Masuy <samuel.masuy@gmail.com>
2015-05-26 02:35:29 -0400

    added db create sqlite tables

--- src/main/java/ninefoo/application/Application.java
+++ src/main/java/ninefoo/application/Application.java
@@ -1,16 +1,18 @@
 package ninefoo.application;
 import ninefoo.lib.LanguageText;
 import ninefoo.view.MainView;
+import ninefoo.model.Project_model;
 
 public class Application {
-	
+
 	// Global Constants
 	public final static String APPLICATION_PATH = "/ninefoo/";
 	public final static LanguageText LANGUAGE_TEXT = new LanguageText();
 	
 	// Main thread
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		Project_model.createDatabase();
 		// Set default language to English
 		LANGUAGE_TEXT.load_en();
 		
