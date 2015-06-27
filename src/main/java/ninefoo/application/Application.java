a450c41f730edbd7e4f88276c5682b51f3c1edd3
Souleymane Guindo <souley.guindo@gmail.com>
2015-06-27 16:31:54 -0400

    Added code for application to be multiple platform eg:mac/windows

--- src/main/java/ninefoo/application/Application.java
+++ src/main/java/ninefoo/application/Application.java
@@ -1,5 +1,8 @@
 package ninefoo.application;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import ninefoo.config.Config;
 import ninefoo.controller.MainController;
 
@@ -11,18 +14,37 @@ import org.apache.logging.log4j.LogManager;
  */
 public class Application {
 
-    // Logger
+	// Logger
 	private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
-	
+
 	// Main thread
 	public static void main(String[] args) {
 
+		//Look and feel for the crossplatform 
+		try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel(
+					UIManager.getCrossPlatformLookAndFeelClassName());
+		} 
+		catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		}
+		catch (ClassNotFoundException e) {
+			// handle exception
+		}
+		catch (InstantiationException e) {
+			// handle exception
+		}
+		catch (IllegalAccessException e) {
+			// handle exception
+		}
+
 		// Welcome logger
 		LOGGER.info("Application started ...");
-				
+
 		// Run the auto load before the Controller starts
 		Config.autoload();
-		
+
 		// Start the application
 		new MainController(String.format("%s - v%s", Config.APPLICATION_NAME, Config.APPLICATION_VERSION));
 	}
