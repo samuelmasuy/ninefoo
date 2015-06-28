390aa030ebc2b565723a72565dda152ea1f3babd
Amir El Bawab <amirbawab@gmail.com>
2015-06-28 02:12:02 -0700

    Removed Database lib

--- src/main/java/ninefoo/lib/database/QueryCustom.java
+++
@@ -1,9 +0,0 @@
-package ninefoo.lib.database;
-
-import java.sql.ResultSet;
-
-public interface QueryCustom {
-	public ResultSet getResult();
-	public String getQuery();
-	public boolean run(String query);
-}
