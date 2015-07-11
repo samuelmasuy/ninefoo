afc91544ca0e90337f993d19b34a3415a7be4d1f
Amir El Bawab <amirbawab@gmail.com>
2015-07-11 15:48:36 -0700

    Added multi dropdown lib

---
+++ src/main/java/ninefoo/application/TestMultiDropdown.java
@@ -0,0 +1,44 @@
+package ninefoo.application;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.Arrays;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import ninefoo.lib.multiDropdown.MultiDropdown;
+
+public class TestMultiDropdown {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		JPanel orgPanel = new JPanel();
+		final MultiDropdown panel = new MultiDropdown("Add dependency", new String[]{"One", "Two"});
+		panel.setPreferredSize(new Dimension(250,200));
+		panel.setBackground(Color.RED);
+		orgPanel.add(panel);
+		frame.add(orgPanel);
+		
+		new Thread(){
+			public void run() {
+				while(true){
+					System.out.println(Arrays.toString(panel.getData()));
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+		}.start();
+		
+		// Configure
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(300, 300));
+		frame.setVisible(true);
+		frame.pack();
+	}
+}
