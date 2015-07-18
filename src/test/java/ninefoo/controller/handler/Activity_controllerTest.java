a5e376eef6b2176412eb276193b5c2aebf8ab011
Samuel Masuy <samuel.masuy@gmail.com>
2015-07-18 14:55:09 -0400

    added test for activity controller

--- src/test/java/ninefoo/controller/handler/Activity_controllerTest.java
+++ src/test/java/ninefoo/controller/handler/Activity_controllerTest.java
@@ -3,9 +3,11 @@ package ninefoo.controller.handler;
 
 import ninefoo.Mocks.MockUpdatableView;
 import ninefoo.config.Config;
+import ninefoo.config.Session;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
@@ -15,10 +17,14 @@ import static org.junit.Assert.*;
 public class Activity_controllerTest {
     private MockUpdatableView mockUpdatableView = new MockUpdatableView();
     private Activity_controller activity_controller;
+    private static Project_controller project_controller;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         Config.autoload();
+        MockUpdatableView mockUpdatableViewTest = new MockUpdatableView();
+        project_controller = new Project_controller(mockUpdatableViewTest);
+        project_controller.createProject("name", "0", "1/1/1111", "1/1/1112", "description");
     }
 
     @Before
@@ -28,15 +34,79 @@ public class Activity_controllerTest {
 
     @Test
     public void testCreateActivity() throws Exception {
-//        activity_controller.createActivity(0, "0",
-//            "label", "1", "1/1/2004",
-//            "1/1/2002", "55", null,
-//            1, new String[]{});
+        Session.getInstance().setProjectId(1);
+        activity_controller.createActivity("label", "description", "5", "6", "7", "8", "100", "11/11/1111", "12/11/1111", 1, new Integer[]{});
+        assertEquals("create activity is successful method", "updateCreateActivity", mockUpdatableView.get_called_method());
+        assertEquals("create activity is successful success", "true", mockUpdatableView.get_success());
+        assertEquals("create activity is successful message", "Activity created successfully!", mockUpdatableView.get_message());
+
+    }
+
+    @Test
+    public void testCreateActivity_minimum_requirements() throws Exception {
+        Session.getInstance().setProjectId(1);
+        activity_controller.createActivity("label", "", "5", "", "", "", "", "11/11/1111", "12/11/1111", 1, new Integer[]{});
+        assertEquals("create activity is successful method", "updateCreateActivity", mockUpdatableView.get_called_method());
+        assertEquals("create activity is successful success", "true", mockUpdatableView.get_success());
+        assertEquals("create activity is successful message", "Activity created successfully!", mockUpdatableView.get_message());
+
+    }
+
+    @Test
+    public void testCreateActivity_no_duration() throws Exception {
+        Session.getInstance().setProjectId(1);
+        activity_controller.createActivity("label", "", "", "", "", "", "", "11/11/1111", "12/11/1111", 1, new Integer[]{});
+        assertEquals("create activity has no duration method", "updateCreateActivity", mockUpdatableView.get_called_method());
+        assertEquals("create activity has no duration success", "false", mockUpdatableView.get_success());
+        assertEquals("create activity has no duration message", "Duration is required.", mockUpdatableView.get_message());
+
+    }
+
+    @Test
+    public void testCreateActivity_invalid_duration() throws Exception {
+        Session.getInstance().setProjectId(1);
+        activity_controller.createActivity("label", "", "xx", "", "", "", "", "11/11/1111", "12/11/1111", 1, new Integer[]{});
+        assertEquals("create activity has invalid duration method", "updateCreateActivity", mockUpdatableView.get_called_method());
+        assertEquals("create activity has invalid duration success", "false", mockUpdatableView.get_success());
+        assertEquals("create activity has invalid duration message", "Duration is not valid.", mockUpdatableView.get_message());
+
+    }
+
+    @Test
+    public void testCreateActivity_invalid_member() throws Exception {
+        Session.getInstance().setProjectId(1);
+        activity_controller.createActivity("label", "", "5", "", "", "", "", "11/11/1111", "12/11/1111", 999, new Integer[]{});
+        assertEquals("create activity has invalid member method", "updateCreateActivity", mockUpdatableView.get_called_method());
+        assertEquals("create activity has invalid member success", "false", mockUpdatableView.get_success());
+        assertEquals("create activity has invalid member message", "An error occurred", mockUpdatableView.get_message());
+
+    }
+
+    @Test
+    public void testCreateActivity_invalid_project() throws Exception {
+        Session.getInstance().setProjectId(99);
+        activity_controller.createActivity("label", "", "5", "", "", "", "", "11/11/1111", "12/11/1111", 1, new Integer[]{});
+        assertEquals("create activity has invalid project method", "updateCreateActivity", mockUpdatableView.get_called_method());
+        assertEquals("create activity has invalid project success", "false", mockUpdatableView.get_success());
+        assertEquals("create activity has invalid project message", "An error occurred", mockUpdatableView.get_message());
+
+    }
+
+    @Test
+    public void testCreateActivity_prereqs() throws Exception {
+        // TODO prereqs that does not exist
+//        Session.getInstance().setProjectId(1);
+//        activity_controller.createActivity("label", "description", "5", "6", "7", "8", "100", "11/11/1111", "12/11/1111", 1, new Integer[]{5});
+//        assertEquals("login is successful method", "updateCreateActivity", mockUpdatableView.get_called_method());
+//        assertEquals("login is successful success", "true", mockUpdatableView.get_success());
+//        assertEquals("login is successful message", "Activity created successfully!", mockUpdatableView.get_message());
 
     }
 
     @Test
     public void testEditActivity() throws Exception {
+        Session.getInstance().setProjectId(1);
+
 
     }
 
