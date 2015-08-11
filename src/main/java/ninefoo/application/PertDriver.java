66416469d42f22cab0c0bc40d7f93034d5a5e2d1
Amir El Bawab <amirbawab@gmail.com>
2015-08-10 21:16:06 -0400

    Pert

--- src/main/java/ninefoo/application/PertDriver.java
+++ src/main/java/ninefoo/application/PertDriver.java
@@ -11,14 +11,35 @@ import ninefoo.lib.pert.Shape.PertShape;
 public class PertDriver {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Pert");
-		PertPanel pertPanel = new PertPanel();
+		PertPanel pertPanel = new PertPanel(8);
 		
-//		pertPanel.addNodeMode();
-		PertShape p1 = pertPanel.addNode(new Point(100, 100));
-		PertShape p2 = pertPanel.addNode(new Point(500, 500));
-		pertPanel.addEdge(p1, p2);
+		PertShape p0 = pertPanel.addNode();
+		PertShape p1 = pertPanel.addNode();
+		PertShape p2 = pertPanel.addNode();
+		PertShape p3 = pertPanel.addNode();
+		PertShape p4 = pertPanel.addNode();
+		PertShape p5 = pertPanel.addNode();
+		PertShape p6 = pertPanel.addNode();
+		PertShape p7 = pertPanel.addNode();
 		
-		pertPanel.dragNodeMode();
+		p0.eventNumber = 1;
+		
+		pertPanel.addEdge(p0, p1);
+		pertPanel.addEdge(p0, p2);
+		pertPanel.addEdge(p0, p3);
+		pertPanel.addEdge(p0, p7);
+
+		pertPanel.addEdge(p1, p4);
+		pertPanel.addEdge(p2, p4);
+
+		pertPanel.addEdge(p3, p5);
+		
+		pertPanel.addEdge(p4, p6);
+		pertPanel.addEdge(p5, p6);
+
+		pertPanel.addEdge(p6, p7);
+
+		pertPanel.prettify();
 		
 		frame.add(pertPanel);
 		frame.setVisible(true);
